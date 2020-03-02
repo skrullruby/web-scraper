9915c5aa4562e1755b555621abbe116cce127bb4
skrullruby <40552974+skrullruby@users.noreply.github.com>
2020-03-02 23:51:17 +0800

    upload files

---
+++ bs4_scraper2.py
@@ -0,0 +1,57 @@
+import urllib.request
+
+import bs4		# turns raw web pages into object hierarchy and provides selectors (like CSS and Xpath does)
+import csv		# simplifies the process of writing data to Comma Separated Values in a file
+
+from urllib.parse import urlparse
+
+# a list of URLs that we want to scrape data from
+pagesToScrape = [
+	'https://www.landvest.com/properties_for_sale/advanced_search/view//Forsale/1/SearchClass/timberland/count/12',
+	'https://www.landvest.com/properties_for_sale/advanced_search/Forsale/1/SearchClass/timberland/NoMLS/PS/orderby/acres/descending/offset/12/count/12',
+	'https://www.landvest.com/properties_for_sale/advanced_search/Forsale/1/SearchClass/timberland/NoMLS/PS/orderby/acres/descending/offset/24/count/12'
+]
+
+# open a file in append mode to write into in the same directory where we ran this script from
+csvfile = open('propertydata.csv', 'w')
+csvwriter = csv.writer(csvfile, delimiter=',')
+
+
+property_arr = [['Name', 'State', 'Amount', 'Acreage', 'REF#', 'Website']]
+
+# loop over our list of URL's one at a time
+for URL in pagesToScrape:
+
+	webpage = urllib.request.urlopen(URL)			# fetch webpage
+	soup = bs4.BeautifulSoup(webpage, "html.parser")		# make an object from the HTML
+
+	parsed_uri = urlparse(URL)
+	hosturl = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+
+	# extract info from soup
+	properties = soup.find_all('li', {'class':'standard-property'})
+
+	for prop in properties:
+		title = prop.find('h4', {'class': 'property-name'})
+		name = title.a.text.strip()
+		
+		info = prop.find('div', {'class': 'property-info'})
+		infos = info.find_all('div', {'class': 'info-row'})
+
+		state = infos[0].text.strip() + infos[1].text.strip()
+		amount = infos[2].text.strip()
+		acres = infos[3].text.strip()
+		ref = infos[4].text.strip()
+		website = hosturl + title.a.get('href')
+
+		property_arr.append([
+			name,
+			state,
+			amount,
+			acres,
+			ref,
+			website
+		])
+
+for prop in property_arr:
+	csvwriter.writerow(prop)		# write a row in the file
